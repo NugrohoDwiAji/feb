1a3a0afbd51ccfce618371804c5f33e860da729b
NugrohoDwiAji <nugrohodwiaji@gmail.com>
2025-09-01 10:10:21 +0800

    merubah warna

--- src/pages/pengumuman.tsx
+++ src/pages/pengumuman.tsx
@@ -28,23 +28,23 @@ export default function Pengumuman() {
 
   return (
     <div className="min-h-screen">
-      <div className="relative h-80 md:h-96 lg:h-[28rem]">
+      <div className="relative h-80 md:h-96 lg:h-[43rem]">
         <img
-          src="/img/banner-pasca.png"
+          src="/img/banner-feb.png"
           alt=""
           className="w-full bg-cover h-full"
         />
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-blue-950/50 flex flex-col justify-center p-10 ">
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center p-10 text-shadow-lg/20">
           <h1 className="text-white text-3xl md:text-4xl lg:text-6xl font-bold">
             Pengumuman
           </h1>
-          <h2 className="text-white mt-3">Pengumuman Fakultas Pascasarjana</h2>
+          <h2 className="text-white mt-3">Pengumuman Fakultas Ekonomi dan Bisnis</h2>
         </div>
       </div>
 
       {/* Main */}
       <main>
-        <h1 className="font-bold text-xl md:text-2xl my-5 text-center ">
+        <h1 className="font-bold text-xl md:text-2xl my-5 text-center text-cyan-400 text-shadow-lg/20 ">
           Pengumuman
         </h1>
         <div className="flex flex-wrap justify-center gap-5 items-center mb-5">
